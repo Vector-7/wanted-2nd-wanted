4488695bddcc395120869d9daf8cd4165f1109c8
Vector-7 <seokbong60@gmail.com>
2022-05-22 11:27:36 +0900

    first init

---
+++ config/settings/local.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DEBUG = True
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY"),
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get("MYSQL_PASSWD"),
+        'HOST': os.environ.get('MYSQL_HOST'),
+        'PORT': os.environ.get('MYSQL_PORT')
+    }
+}
+CACHES = {
+    'default': {
+        "BACKEND": "django_redis.cache.RedisCache",
+        'LOCATION': os.environ.get('REDIS_HOST')
+    }
+}
+
+'''
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+'''
